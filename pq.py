2ceba6b887b692721930d121afb7b45a672e0990
Chiaki <Chiaki530@outlook.com>
2019-02-24 01:46:14 +0800

    add mpq1

--- pq.py
+++ pq.py
@@ -2,20 +2,25 @@ from __future__ import division
 from __future__ import print_function
 import numpy as np
 from scipy.cluster.vq import vq, kmeans2
+import tqdm
+import numba as nb
 
 
 class PQ(object):
     def __init__(self, M, Ks, verbose=True):
         assert 0 < Ks <= 2 ** 32
         self.M, self.Ks, self.verbose = M, Ks, verbose
-        self.code_dtype = np.uint8 if Ks <= 2 ** 8 else (np.uint16 if Ks <= 2 ** 16 else np.uint32)
+        self.code_dtype = np.uint8 if Ks <= 2 ** 8 else (
+            np.uint16 if Ks <= 2 ** 16 else np.uint32)
         self.codewords = None
         self.Ds = None
         self.Dim = -1
 
+    # M: number of sub-quantizers; Ks: number of centroids in each sub-quantizer.
     def class_message(self):
         return "Subspace PQ, M: {}, Ks : {}, code_dtype: {}".format(self.M, self.Ks, self.code_dtype)
 
+    # fit: K-means training
     def fit(self, vecs, iter):
         assert vecs.dtype == np.float32
         assert vecs.ndim == 2
@@ -25,16 +30,22 @@ class PQ(object):
 
         reminder = D % self.M
         quotient = int(D / self.M)
-        dims_width = [quotient + 1 if i < reminder else quotient for i in range(self.M)]
+        # [q + 1, ..., q + 1, q, ..., q]
+        dims_width = [quotient + 1 if i <
+                      reminder else quotient for i in range(self.M)]
         self.Ds = np.cumsum(dims_width)     # prefix sum
         self.Ds = np.insert(self.Ds, 0, 0)  # insert zero at beginning
 
         # [m][ks][ds]: m-th subspace, ks-the codeword, ds-th dim
-        self.codewords = np.zeros((self.M, self.Ks, np.max(self.Ds)), dtype=np.float32)
+        self.codewords = np.zeros(
+            (self.M, self.Ks, np.max(self.Ds)), dtype=np.float32)
         for m in range(self.M):
             if self.verbose:
-                print("#    Training the subspace: {} / {}, {} -> {}".format(m, self.M, self.Ds[m], self.Ds[m+1]))
+                print("#    Training the subspace: {} / {}, {} -> {}".format(m,
+                                                                             self.M, self.Ds[m], self.Ds[m+1]))
+            # All columns and specific roes
             vecs_sub = vecs[:, self.Ds[m]:self.Ds[m+1]]
+            # dims_width[m] = self.Ds[m+1] - self.Ds[m]
             self.codewords[m, :, :self.Ds[m+1] - self.Ds[m]], _ = kmeans2(
                 vecs_sub, self.Ks, iter=iter, minit='points')
 
@@ -49,6 +60,8 @@ class PQ(object):
         codes = np.empty((N, self.M), dtype=self.code_dtype)
         for m in range(self.M):
             vecs_sub = vecs[:, self.Ds[m]: self.Ds[m+1]]
+            # Last parameter: fix the length (because the #dimensions of each subspace is not equivalent).
+            # vq from scipy: distance measure?
             codes[:, m], _ = vq(vecs_sub,
                                 self.codewords[m, :, :self.Ds[m+1] - self.Ds[m]])
 
@@ -62,9 +75,96 @@ class PQ(object):
 
         vecs = np.empty((N, self.Dim), dtype=np.float32)
         for m in range(self.M):
-            vecs[:, self.Ds[m]: self.Ds[m+1]] = self.codewords[m, codes[:, m], :self.Ds[m+1] - self.Ds[m]]
+            vecs[:, self.Ds[m]: self.Ds[m+1]] = self.codewords[m,
+                                                               codes[:, m], :self.Ds[m+1] - self.Ds[m]]
 
         return vecs
 
     def compress(self, vecs):
         return self.decode(self.encode(vecs))
+
+
+class MPQ(object):
+    @nb.jit
+    def __init__(self, numTable, M, Ks, verbose=True):
+        self.numTable, self.M, self.Ks, self.verbose = numTable, M, Ks, verbose
+        self.tables = []
+        self.code_dtype = np.uint8 if Ks <= 2 ** 8 else (
+            np.uint16 if Ks <= 2 ** 16 else np.uint32)
+        for _ in nb.prange(self.numTable):
+            self.tables.append(PQ(M=self.M, Ks=self.Ks, verbose=False))
+
+    def class_message(self):
+        return "#PQ Table: {}, ".format(self.numTable) + self.tables[0].class_message()
+
+    def fit(self, vecs, iter):
+        print('\n# Start training...')
+        if(self.verbose):
+            for i in tqdm.tqdm(nb.prange(self.numTable)):
+                self.tables[i].fit(vecs, iter)
+        else:
+            for i in nb.prange(self.numTable):
+                self.tables[i].fit(vecs, iter)
+        print('# Training finish!\n')
+
+    def encode(self, vecs):
+        assert vecs.dtype == np.float32
+        assert vecs.ndim == 2
+        N, D = vecs.shape
+        codes = np.empty(
+            (self.numTable, N, self.tables[0].M), dtype=self.code_dtype)
+        for i in range(self.numTable):
+            codes[i, :, :] = self.tables[i].encode(vecs)
+        return codes
+
+    def decode(self, codes):
+        assert codes.ndim == 3
+        nTable, N, M = codes.shape
+        assert nTable == self.numTable
+        vecs = np.empty(
+            (self.numTable, N, self.tables[0].Dim), dtype=np.float32)
+        for i in range(self.numTable):
+            vecs[i, :, :] = self.tables[i].decode(codes[i, :, :])
+        return np.array(vecs)
+
+    def compress(self, vecs):
+        return [t.compress(vecs) for t in self.tables]
+
+
+class MPQ1(object):
+    @nb.jit
+    def __init__(self, numTable, M, Ks, verbose=True):
+        self.numTable, self.M, self.Ks, self.verbose = numTable, M, Ks, verbose
+        self.tables = []
+        self.code_dtype = np.uint8 if Ks <= 2 ** 8 else (
+            np.uint16 if Ks <= 2 ** 16 else np.uint32)
+        for _ in nb.prange(self.numTable):
+            self.tables.append(PQ(M=self.M, Ks=self.Ks, verbose=False))
+
+    def class_message(self):
+        return "#PQ Table: {}, ".format(self.numTable) + self.tables[0].class_message()
+
+    def fit(self, vecs, iter):
+        print('\n# Start training...')
+
+        # generate (self.numTable) ways of index permutations and store it
+        self.permutations = [np.random.rand(
+            vecs.shape[1]).argsort() for i in nb.prange(self.numTable)]
+        if(self.verbose):
+            for i in tqdm.tqdm(nb.prange(self.numTable)):
+                self.tables[i].fit(vecs[:, self.permutations[i]], iter)
+        else:
+            for i in nb.prange(self.numTable):
+                self.tables[i].fit(vecs[:, self.permutations[i]], iter)
+        print('# Training finish!\n')
+
+    def encode(self, vecs):
+        assert vecs.dtype == np.float32
+        assert vecs.ndim == 2
+        N, D = vecs.shape
+        codes = np.empty(
+            (self.numTable, N, self.tables[0].M), dtype=self.code_dtype)
+        for i in range(self.numTable):
+            codes[i, :, :] = self.tables[i].encode(
+                vecs[:, self.permutations[i]])
+        return codes
---
+++ run_mpq.py
@@ -0,0 +1,129 @@
+from sorter import *
+from transformer import *
+from vecs_io import loader
+from prettytable import PrettyTable
+import time
+
+
+def chunk_encode(mpq, vecs):
+    chunk_size = 1000000
+    encoded_vecs = np.empty(
+        shape=(mpq.numTable, len(vecs), mpq.M), dtype=np.float32)
+
+    for h in range(mpq.numTable):
+        # for i in tqdm.tqdm(range(math.ceil(len(vecs) / chunk_size))):
+        for i in range(math.ceil(len(vecs) / chunk_size)):
+            encoded_vecs[h, i * chunk_size: (i + 1) * chunk_size, :] \
+                = mpq.tables[h].encode(vecs[i * chunk_size: (i + 1) * chunk_size, :].astype(dtype=np.float32))
+
+    return encoded_vecs
+
+
+def execute(mpq, X, T, Q, G, metric, train_size=100000):
+    f = open('result.txt', 'w')
+    f.write('################################################################')
+    f.write('################################################################\n')
+    f.write('################################################################')
+    f.write('################################################################\n')
+    if T is None:
+        f.write('# Training data size: {t}; query size: {q}.\n'.format(
+            t=X.shape, q=Q.shape))
+    else:
+        f.write('# Training data size: {t}; query size: {q}.\n'.format(
+            t=T.shape, q=Q.shape))
+    f.write('# Metric: {m}, #PQ Tables:{t}.\n'.format(
+        m=metric, t=mpq.numTable))
+    start_time = time.time()
+    np.random.seed(123)
+    print("# Ranking metric {}".format(metric))
+    print("# " + mpq.class_message())
+    if T is None:
+        mpq.fit(X[:train_size].astype(dtype=np.float32), iter=20)
+    else:
+        mpq.fit(T.astype(dtype=np.float32), iter=20)
+    train_time = time.time()
+
+    print('# Encoding dataset and queries...')
+    vecs_encoded = chunk_encode(mpq, X)
+    query_encoded = chunk_encode(mpq, Q)
+
+    # print(vecs_encoded.shape)
+    # print(query_encoded.shape)
+    # print(Q.shape)
+
+    encode_time = time.time()
+
+    query_encoded = np.transpose(query_encoded, (1, 0, 2))
+    # print(query_encoded.shape)
+    # print('{}'.format(query_encoded.shape[0]==Q.shape[0]))
+
+    print("# Sorting items...")
+    Ts = [2 ** i for i in range(2 + int(math.log2(len(X))))]
+    recalls = BatchSorter(vecs_encoded, query_encoded, X,
+                          G, Ts, metric='multitable', batch_size=200).recall()
+    print("# Finish searching!\n")
+
+    finish_time = time.time()
+    f.write('# Training time:{t}.\n'.format(t=train_time - start_time))
+    f.write('# Encoding time:{t}.\n'.format(t=encode_time - train_time))
+    f.write('# Query time:{t}.\n'.format(t=finish_time - encode_time))
+    f.write('################################################################')
+    f.write('################################################################\n')
+    f.write('################################################################')
+    f.write('################################################################\n')
+
+    table = PrettyTable()
+    table.field_names = ["Expected Items", "Overall time",
+                         "AVG Recall", "AVG precision", "AVG error", "AVG items"]
+    for i, (t, recall) in enumerate(zip(Ts, recalls)):
+        table.add_row([2 ** i, 0, recall, recall * len(G[0]) / t, 0, t])
+    print(table)
+    f.write(table.get_string())
+    f.close()
+    # print("expected items, overall time, avg recall, avg precision, avg error, avg items")
+    # for i, (t, recall) in enumerate(zip(Ts, recalls)):
+    #     print("{}, {}, {}, {}, {}, {}".format(
+    #         2**i, 0, recall, recall * len(G[0]) / t, 0, t))
+
+
+def parse_args():
+    # override default parameters with command line parameters
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='Process input method and parameters.')
+    parser.add_argument('--num_table', type=int,
+                        help='choose the number of PQ tables')
+    parser.add_argument('--dataset', type=str, help='choose data set name')
+    parser.add_argument('--topk', type=int,
+                        help='required topk of ground truth')
+    parser.add_argument('--metric', type=str, help='metric of ground truth')
+    parser.add_argument('--num_codebook', type=int, help='number of codebooks')
+    parser.add_argument(
+        '--Ks', type=int, help='number of centroids in each quantizer')
+    args = parser.parse_args()
+    return args.dataset, args.topk, args.num_codebook, args.Ks, args.metric, args.num_table
+
+
+if __name__ == '__main__':
+    dataset = 'netflix'
+    topk = 20
+    codebook = 4
+    Ks = 256
+    metric = 'product'
+    num_table = 2
+
+    # override default parameters with command line parameters
+    import sys
+    if len(sys.argv) > 3:
+        dataset, topk, codebook, Ks, metric, num_table = parse_args()
+    else:
+        import warnings
+        warnings.warn("Using default Parameters ")
+    print("# Parameters: #tables = {}, dataset = {}, topK = {}, codebook = {}, Ks = {}, metric = {}"
+          .format(num_table, dataset, topk, codebook, Ks, metric))
+
+    # Base, training, query, ground truth
+    X, T, Q, G = loader(dataset, topk, metric, folder='data/')
+    # pq, rq, or component of norm-pq
+    quantizer = MPQ(numTable=num_table, M=codebook, Ks=Ks)
+    execute(quantizer, X, T, Q, G, metric)
